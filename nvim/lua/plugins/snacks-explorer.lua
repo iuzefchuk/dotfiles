b4deded47a3ac0d423605a0b9a3c41c308d17f96
iuzefchuk <yuzefchukk@gmail.com>
2026-07-26 13:50:28 +0300

    removed left release override in nvim

--- nvim/lua/plugins/snacks-explorer.lua
+++ nvim/lua/plugins/snacks-explorer.lua
@@ -5,15 +5,6 @@ return {
       sources = {
         explorer = {
           hidden = true, -- show dotfiles (gitignored files stay hidden)
-          win = {
-            list = {
-              keys = {
-                -- Single click opens a file / toggles a directory. By default the
-                -- explorer only moves the cursor on click and needs <cr> to open.
-                ["<LeftRelease>"] = "confirm",
-              },
-            },
-          },
         },
       },
     },

045457245f6ce61b1011632aad4e55730c3b270a
iuzefchuk <yuzefchukk@gmail.com>
2026-07-28 15:10:26 +0300

    autofill disable

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -4,6 +4,11 @@ return {
   opts = {
     completion = {
       menu = { auto_show = false }, -- no auto-popup; Ctrl+Space shows it on demand
+      ghost_text = { enabled = false }, -- no inline grey suggestion (LazyVim turns this on via vim.g.ai_cmp)
+      trigger = {
+        show_on_keyword = false, -- don't start completing just because you typed a word character
+        show_on_trigger_character = false, -- ...or a trigger character like `.`
+      },
     },
   },
 }

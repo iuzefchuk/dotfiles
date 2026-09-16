ce8302da2528c71e6beee2fb68a1806a71461867
iuzefchuk <yuzefchukk@gmail.com>
2026-09-16 16:07:01 +0300

    fixed prob

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -3,3 +3,10 @@ if vim.fn.argc(-1) == 0 then
 end
 
 vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.hl.on_yank() end })
+
+vim.api.nvim_create_autocmd("SwapExists", {
+  callback = function()
+    local info = vim.fn.swapinfo(vim.v.swapname)
+    if not info.error and info.pid == 0 and info.dirty == 0 then vim.v.swapchoice = "d" end
+  end,
+})
